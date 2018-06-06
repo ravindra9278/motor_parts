3748816c5fcdd0d4808ca9f7055a3a4f59012b4e
Ravindra <ravindra@zuppler.com>
2018-06-06 07:30:14 +0530

    change readme dile

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  def index
+  end
+  def about
+  end
+  def contact
+  end
+  def faq
+  end
+  def services
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  
+  # Set Views/Home/index is the main page
+  root 'home#index'
+  get 'home/index', to: 'home#index'
+  
+  get 'home/about'
+  get 'home/contact'
+  get 'home/faq'
+  get 'home/services'
+end
